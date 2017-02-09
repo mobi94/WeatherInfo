7bca4136ac04a995460c19af696921a57335534d
Sergey Stasyuk <stas.serg94@gmail.com>
2017-02-09 13:16:21 +0200

    Collapsing toolbar layout added, tab layout added

---
+++ app/src/main/java/com/breezee/sergeystasyuk/weatherinfo/ForecastFragment.java
@@ -0,0 +1,80 @@
+package com.breezee.sergeystasyuk.weatherinfo;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by User on 09.02.2017.
+ */
+
+public class ForecastFragment extends Fragment {
+
+    String[] names = { "Рыжик", "Барсик", "Мурзик", "Мурка", "Васька",
+            "Томасина", "Кристина", "Пушок", "Дымка", "Кузя",
+            "Китти", "Масяня", "Симба"	};
+
+    RecyclerView recyclerView;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_forecast, container, false);
+
+        ForecastFragment.ItemArrayAdapter itemArrayAdapter = new ForecastFragment.ItemArrayAdapter(R.layout.list_item);
+        recyclerView = (RecyclerView) view.findViewById(R.id.listview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(itemArrayAdapter);
+
+        return view;
+    }
+
+    public class ItemArrayAdapter extends RecyclerView.Adapter<ItemArrayAdapter.ViewHolder> {
+
+        //All methods in this adapter are required for a bare minimum recyclerview adapter
+        private int listItemLayout;
+        // Constructor of the class
+        ItemArrayAdapter(int layoutId) {
+            listItemLayout = layoutId;
+        }
+
+        // get the size of the list
+        @Override
+        public int getItemCount() {
+            return names.length;
+        }
+
+
+        // specify the row layout file and click for each row
+        @Override
+        public ItemArrayAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(listItemLayout, parent, false);
+            return new ItemArrayAdapter.ViewHolder(view);
+        }
+
+        // load data in each row element
+        @Override
+        public void onBindViewHolder(final ItemArrayAdapter.ViewHolder holder, final int listPosition) {
+            TextView item = holder.item;
+            item.setText(names[listPosition]);
+        }
+
+        // Static inner class to initialize the views of rows
+        class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+            TextView item;
+            ViewHolder(View itemView) {
+                super(itemView);
+                itemView.setOnClickListener(this);
+                item = (TextView) itemView.findViewById(R.id.row_item);
+            }
+            @Override
+            public void onClick(View view) {
+            }
+        }
+    }
+}
--- app/src/main/java/com/breezee/sergeystasyuk/weatherinfo/MainActivity.java
+++ app/src/main/java/com/breezee/sergeystasyuk/weatherinfo/MainActivity.java
@@ -1,27 +1,45 @@
 package com.breezee.sergeystasyuk.weatherinfo;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(
+                R.id.collapse_toolbar);
+        collapsingToolbar.setTitleEnabled(false);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.app_name);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -32,70 +50,67 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+//                switch (tab.getPosition()) {
+//                    case 0:
+//                        showToast("One");
+//                        break;
+//                    case 1:
+//                        showToast("Two");
+//                        ...
+//                }
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {}
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {}
+        });
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFrag(new CurrentConditionFragment(), "Current condition");
+        adapter.addFrag(new ForecastFragment(), "5 days forecast");
+        viewPager.setAdapter(adapter);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+    static class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment>  fragmentList = new ArrayList<>();
+        private final List<String> fragmentTitleList = new ArrayList<>();
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
         }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+        public void addFrag(Fragment fragment, String title) {
+            fragmentList.add(fragment);
+            fragmentTitleList.add(title);
+        }
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentTitleList.get(position);
+        }
     }
 }
